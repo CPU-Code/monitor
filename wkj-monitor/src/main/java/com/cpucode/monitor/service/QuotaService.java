8bae659613599d82249b2a40f1cc5e0b52f0920d
cpuCode <923992029@qq.com>
2021-10-02 14:53:49 +0800

    获取所有报文主题

--- wkj-monitor/src/main/java/com/cpucode/monitor/service/QuotaService.java
+++ wkj-monitor/src/main/java/com/cpucode/monitor/service/QuotaService.java
@@ -3,6 +3,8 @@ package com.cpucode.monitor.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cpucode.monitor.entity.QuotaEntity;
 
+import java.util.List;
+
 /**
  * @author : cpucode
  * @date : 2021/10/1 15:38
@@ -10,4 +12,9 @@ import com.cpucode.monitor.entity.QuotaEntity;
  * @csdn : https://blog.csdn.net/qq_44226094
  */
 public interface QuotaService extends IService<QuotaEntity> {
+    /**
+     * 获取所有报文主题
+     * @return
+     */
+    List<String> getAllSubject();
 }
--- wkj-monitor/src/main/java/com/cpucode/monitor/service/impl/QuotaServiceImpl.java
+++ wkj-monitor/src/main/java/com/cpucode/monitor/service/impl/QuotaServiceImpl.java
@@ -1,9 +1,14 @@
 package com.cpucode.monitor.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cpucode.monitor.entity.QuotaEntity;
 import com.cpucode.monitor.mapper.QuotaMapper;
+import com.cpucode.monitor.service.QuotaService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author : cpucode
@@ -12,6 +17,17 @@ import org.springframework.stereotype.Service;
  * @csdn : https://blog.csdn.net/qq_44226094
  */
 @Service
-public class QuotaServiceImpl extends ServiceImpl<QuotaMapper, QuotaEntity> {
+public class QuotaServiceImpl extends ServiceImpl<QuotaMapper, QuotaEntity> implements QuotaService {
+    /**
+     * 获取所有报文主题
+     * @return 报文主题集合
+     */
+    @Override
+    public List<String> getAllSubject() {
+        QueryWrapper<QuotaEntity> wrapper = new QueryWrapper<>();
+        wrapper.lambda().select(QuotaEntity::getSubject);
 
+        return this.list(wrapper).stream()
+                .map(q -> q.getSubject()).collect(Collectors.toList());
+    }
 }
