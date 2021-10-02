6cb0405db9d85fa807a5fcf47731f512ec50eb0f
cpuCode <923992029@qq.com>
2021-10-02 17:39:55 +0800

    根据主题查询指标配置列表

--- wkj-monitor/src/main/java/com/cpucode/monitor/mapper/QuotaMapper.java
+++ wkj-monitor/src/main/java/com/cpucode/monitor/mapper/QuotaMapper.java
@@ -3,6 +3,9 @@ package com.cpucode.monitor.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cpucode.monitor.entity.QuotaEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * @author : cpucode
@@ -12,4 +15,13 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface QuotaMapper extends BaseMapper<QuotaEntity> {
+    /**
+     * 根据主题查询指标配置列表
+     * @param subject
+     * @return
+     */
+    @Select("select *" +
+            "from tb_quota" +
+            "where subject = #{subject}")
+    List<QuotaEntity> selectBySubject(String subject);
 }
